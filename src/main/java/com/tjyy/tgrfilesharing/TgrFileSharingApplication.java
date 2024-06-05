0f4884caf078511f000dcbeef5238627d182c98e
tianjiangyu <161840237@nuaa.edu.cn>
2024-06-05 23:46:44 +0800

    init project

---
+++ src/main/java/com/tjyy/tgrfilesharing/TgrFileSharingApplication.java
@@ -0,0 +1,13 @@
+package com.tjyy.tgrfilesharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgrFileSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgrFileSharingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tjyy/tgrfilesharing/TgrFileSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjyy.tgrfilesharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgrFileSharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
